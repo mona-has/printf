4492455ad7e029d33e6707d2e8748a5ac263b552
Mooona <Mona.7assan.ahmed@gmail.com>
2023-09-19 12:57:04 +0200

    updated

--- para_fun.c
+++ para_fun.c
@@ -19,8 +19,8 @@ void init_para(para_s *para, va_list ptr)
 	para->hashtag_f = 0;
 	para->zero_f = 0;
 	para->minus_f = 0;
-	para->width_f = 0;
-	para->precision_f = UINT_MAX;
+	para->width = 0;
+	para->precision = UINT_MAX;
 	para->h_modifier = 0;
 	para->l_modifier = 0;
 }
